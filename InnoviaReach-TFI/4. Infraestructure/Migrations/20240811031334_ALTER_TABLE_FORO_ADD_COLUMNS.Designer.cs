af93ffe78589ee0062e43c7536b4feb341ce809f
GonzaIba <gonzaloiba.28.00@gmail.com>
2024-08-11 13:46:54 -0300

    Se agregan migrations

--- InnoviaReach-TFI/4. Infraestructure/Migrations/20240811031334_ALTER_TABLE_FORO_ADD_COLUMNS.Designer.cs
+++ InnoviaReach-TFI/4. Infraestructure/Migrations/20240811031334_ALTER_TABLE_FORO_ADD_COLUMNS.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace _4._Infraestructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240811031334_ALTER_TABLE_FORO_ADD_COLUMNS")]
+    partial class ALTER_TABLE_FORO_ADD_COLUMNS
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
